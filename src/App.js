8a39317437897c7a03a6bd4c8dd5c543498dd3ed
yiti_ <yitijiang@gmail.com>
2020-05-07 14:49:50 -0400

    added nav and clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import './App.css'
+import Clock from './components/Clock.js'
+
+function Icon(props){
+  console.log(props);
+  return(
+    <div className={props.className}>
+      <img src={require("./assets/" + props.src)} alt={props.name}/>
+      {props.name}
+    </div>
+  );
+}
+
+function Nav(props){
+  return (
+  <div className="flex flex-row justify-between pt-6 px-12">
+    <div className="flex flex-row text-center items-end">
+      <Icon className="mr-12" src="nav/timer.svg" name="Timer"/>
+      <Icon src="nav/calendar.svg" name="Calendar"/>
+    </div>
+    <div className="flex flex-row text-center items-end">
+      <Icon className="mr-12" src="nav/statistic.svg" name="statistic"/>
+      <Icon src="nav/setting.svg" name="setting"/>
+    </div>
+  </div>
+  );
+}
 
 function App() {
   return (
-    <div className="test">hello world</div>
+    <div>
+      <Nav/>
+      <div className="text-5xl">
+        Current Time
+      </div>
+      <Clock />
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors:{
+      white: "#FFFFFF",
+      purple:{
+        1: "#675D78"
+      },
+    },
+    extend: {
+    },
+  },
+  variants: {},
+  plugins: [],
+}
