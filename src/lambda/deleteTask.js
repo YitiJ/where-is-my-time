13e358c146c4d59f72c51d5aeb986777385480ed
yiti_ <yitijiang@gmail.com>
2020-05-13 21:42:15 -0400

    added edit & delete page to backend

---
+++ src/lambda/deleteTask.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+import Task from './Models/Task.js'
+import db from './server.js'
+
+exports.handler = async (event,context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    try{
+        const data = JSON.parse(event.body);
+        const task = await Task.findById(data.taskID);
+        if(task == null){
+            throw("TaskID does not exist. Cannot add history.");
+        }
+        const id = mongoose.Types.ObjectId();
+        const history = {
+                _id: id,
+                startTime: data.startTime,
+                duration: data.duration,
+                task: data.taskID
+
+            };
+        const response ={
+                msg:"History added sucessfully",
+                data: history
+            };
+        await TaskHistory.create(history);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        };
+    } catch (err){
+        return {
+            statusCode: 500,
+            body: JSON.stringify({msg:err.message})
+        };
+    }
+}
